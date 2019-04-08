fca838282fb3690df6f6dc37717119fa1bc848ad
reactjr <kauka659@gmail.com>
2019-04-08 10:06:45 +0700

    fix search

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { StyleSheet, TextInput, Button, FlatList, ActivityIndicator, Text, View, TouchableOpacity  } from 'react-native';
+import { StyleSheet, StatusBar, Button, FlatList, ActivityIndicator, Text, View, TouchableOpacity  } from 'react-native';
+import { SearchBar, Header, Divider } from 'react-native-elements';
 
 export default class Home extends React.Component {
   static navigationOptions = {
-    title:'Beranda'
+    header: null
   };
   constructor(props){
     super(props);
-    this.state = { isLoading: true}
-    this.state = { parsingDisease: ''}
+    this.state = { 
+      isLoading: true,
+      parsingDisease: '',
+      search: '',
+      error: null
+    }
+    this.arayHolder = []
   }
 
   componentDidMount(){   
@@ -17,14 +23,32 @@ export default class Home extends React.Component {
       .then((responseJson) => {
         this.setState({
           isLoading: false,
-          dataSource: responseJson.sort((a, b) => a.Disease.localeCompare(b.Disease)) //sorting it just layk that
+          dataSource: responseJson.sort((a, b) => a.Disease.localeCompare(b.Disease)), //sorting it just layk that
+          error : responseJson.error || null          
         }, function(){
+          this.arrayHolder = responseJson.sort((a, b) => a.Disease.localeCompare(b.Disease))
         });
       })
       .catch((error) =>{
-        console.error(error);
+        this.setState({ error, loading: false })
       });
   } 
+  searchFilterFunction(text) {  
+    //passing inserted text in searchbar  
+    const newData = this.arrayHolder.filter(function(item) {   
+      //applying filter    
+      const itemData = item.Disease ? item.Disease.toUpperCase(): ''.toUpperCase();   
+       const textData = text.toUpperCase();        
+       return itemData.indexOf(textData) > -1;    
+    });    
+  
+    this.setState({ 
+      //setting the filtered newData on dataSource
+      dataSource : newData,
+      search : text
+   });  
+  };
+
 
   render(){    
     const { navigate } = this.props.navigation;
@@ -34,16 +58,21 @@ export default class Home extends React.Component {
           <ActivityIndicator/>
         </View>
       )
-    } else{
+    } else {
       return(
         <View style={styles.container}>
-          <View style={styles.searchBar}>
-            <TextInput
-              placeholder="Cari info penyakit"
-              onChangeText={(text) => this.setState({text})}
-            />
-            <Button title='Search' style={styles.buttonSearch}/>
-          </View>
+        <StatusBar backgroundColor="rgb(4,38,63)" translucent={true}/>
+        <Header   
+          backgroundColor = "rgb(4,38,63)"   
+          barStyle = 'light-content'    
+          centerComponent ={{text: 'Disease Info', style:{color: '#FFFFFF', fontSize: 24}}}/>
+       <SearchBar
+          lightTheme
+          searchIcon={{size: 20}}
+          placeholder="Search.."
+          onChangeText={text => this.searchFilterFunction(text)}
+          value ={this.state.search}/>
+
           <FlatList 
             data={this.state.dataSource}
             renderItem={({item}) => 
@@ -51,11 +80,13 @@ export default class Home extends React.Component {
                 <TouchableOpacity onPress={() => navigate('detail', { parsingDisease : item.Disease })}>
                   <Text style={styles.listText} >{item.Disease}</Text>
                 </TouchableOpacity>
-            </View>}
+                <Divider/>
+            </View>
+            }
             keyExtractor={({id}, index) => id}
           />
-          <Button style={styles.submitButton} title='Sorting'
-          onPress={() => navigate('sort')}/>
+          {/* <Button style={styles.submitButton} title='Sorting'
+          onPress={() => navigate('sort')}/> */}
           <Button style={styles.submitButton} title='Tambah Informasi Penyakit'
           onPress={() => navigate('insert')}/>
         </View>
@@ -67,10 +98,10 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    padding: 5
   }, 
-  listText :{
-    fontSize: 24
+  listText:{
+    fontSize: 26,
+    margin: 10,
   },
   buttonSearch:{
     width:10,
--- src/screens/Sorting.js
+++ src/screens/Sorting.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, View, Text, FlatList, Button } from "react-native";
-import { SearchBar, Header } from 'react-native-elements';
+import { StyleSheet, View, Text, FlatList, StatusBar } from "react-native";
+import { SearchBar, Header, Divider } from 'react-native-elements';
 
 export default class Myapp extends Component {
   static navigationOptions = {
@@ -8,12 +8,30 @@ export default class Myapp extends Component {
   }
   constructor(props) {
     super(props);
-    this.state = { isLoading: true, search: '' };
+    this.state = { isLoading: true, search: '',  error: null };
+    this.arrayHolder = [];
   }
 
-  updateSearch = search => {
-    this.setState({search})
+  search = text => {
+    console.log(text)
   }
+
+  searchFilterFunction(text) {  
+    //passing inserted text in searchbar  
+    const newData = this.arrayHolder.filter(function(item) {   
+      //applying filter    
+      const itemData = item.Disease ? item.Disease.toUpperCase(): ''.toUpperCase();   
+       const textData = text.toUpperCase();        
+       return itemData.indexOf(textData) > -1;    
+    });    
+  
+    this.setState({ 
+      //setting the filtered newData on dataSource
+      dataSource : newData,
+      search : text
+   });  
+  };
+
   componentDidMount() {
     return fetch("http://medped.achmadekojulianto.com/index.php/api/disease")
       .then(response => response.json())
@@ -21,38 +39,37 @@ export default class Myapp extends Component {
         this.setState(
           {
             isLoading: false,
-            dataSource: responseJson.sort((a, b) => a.Disease.localeCompare(b.Disease))
-          },
-          function(){
-
-          }
-        )
+            dataSource: responseJson.sort((a, b) => a.Disease.localeCompare(b.Disease)),
+            error : responseJson.error || null
+          }, function (){
+            this.arrayHolder = responseJson.sort((a, b) => a.Disease.localeCompare(b.Disease))
+          })
       })
       .catch(error => {
-        console.error(error);
+        this.setState({ error, loading: false })
       });
   }
   render() {
-    const {search} = this.state
     return (
-      <View>
-        <Header  
-        barStyle='light-content'      
-        backgroundColor= '#DAE1E9'
-        centerComponent ={{text: 'Disease Info', style:{color: '#333333', fontSize: 24}}}/>
+      <View style={{styles}}>        
+      <StatusBar backgroundColor="rgb(4,38,63)" translucent={true}/>
+        <Header   
+          backgroundColor = "rgb(4,38,63)"   
+          barStyle = 'light-content'    
+          centerComponent ={{text: 'Disease Info', style:{color: '#FFFFFF', fontSize: 24}}}/>
         <SearchBar
-          style={{ backgroundColor: '#AAAAAA'}}
-          placeholder="Type Here..."
-          onChangeText={this.updateSearch}
-          value={search}
-          outer
+          lightTheme
+          searchIcon={{size: 20}}
+          placeholder="Search.."
+          onChangeText={text => this.searchFilterFunction(text)}
+          value ={this.state.search}
         />
          <FlatList 
             data={this.state.dataSource}
             renderItem={({item}) =>                         
             <View>
-              <Text>{item.id}</Text>
-              <Text>{item.Disease}</Text>  
+              <Text style={styles.listText}>{item.Disease}</Text>  
+              <Divider/>
             </View>                       
             }
             keyExtractor={({Disease}, index) => Disease}
@@ -62,17 +79,17 @@ export default class Myapp extends Component {
   }
 }
 
+
 const styles = StyleSheet.create({
+
   MainContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingTop: Platform.OS === "ios" ? 20 : 0
   },
 
-  text: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  }
+  listText:{
+    fontSize: 26,
+    margin: 15,
+  },
 });
